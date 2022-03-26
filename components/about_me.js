a693ebf74a9c1d05159c10f864d6c2760632672c
Colin Alexander <colintalex@gmail.com>
2022-03-26 00:12:24 -0600

    wip

--- components/about_me.js
+++ components/about_me.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import styles from './about_me.module.css'
 import Card from "./card";
@@ -6,6 +7,8 @@ export default function AboutMe (param) {
   return (
     <Card>
       <h2 className={styles.title}>About Me</h2>
+      <Image src="https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg"
+        width="100" height="100" className={styles.avatar}/>
     </Card>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      "wallpaperaccess.com",
+      "mybayutcdn.bayut.com",
+      "www.teahub.io",
+      "www.vhv.rs",
+      "thumbs.dreamstime.com",
+    ],
+  },
   reactStrictMode: true,
-}
+};
 
 module.exports = nextConfig
