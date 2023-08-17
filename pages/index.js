450caa76e377078dffb6fe76a1e0b3cb854fb529
Colin Alexander <colintalex@gmail.com>
2023-08-16 20:23:01 -0600

    Mobile CSS

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,17 @@ export default function Home() {
   const [value3, setValue3] = useState(0.14)
 
   let val1;
-  
   useEffect(() => {
-
-      // Initialize Isomer
-    var iso = new Isomer(document.getElementById("box_canvas"), { useWorker: false });
+    // Initialize Isomer
+    var ctx = document.getElementById("box_canvas");
+    var iso = new Isomer(ctx, { useWorker: false });
 
     // Define block parameters
-    var size = 0.4; // Adjust this value to change the size of the blocks
+    var size = 0.25; // Adjust this value to change the size of the blocks
     var count = 12; // Adjust this value to change the number of blocks
+    var initialX = 0; // Adjust this value to change the initial X position of the blocks
+    var initialY = 0; // Adjust this value to change the initial Y position of the blocks
+    var initialZ = 0.5; // Adjust this value to change the initial Z height of the blocks
 
     // Define constants
     const PI = Math.PI;
@@ -28,23 +30,32 @@ export default function Home() {
     var delayFactor1 = 3; // Adjust this value to change the speed of the ripple
     var delayFactor2 = 2; // Adjust this value to change the speed of the ripple
     var amplitude1 = 0.1; // Adjust this value to change the amplitude of the oscillation
-    var amplitude2 = .2; // Adjust this value to change the amplitude of the oscillation
+    var amplitude2 = 0.2; // Adjust this value to change the amplitude of the oscillation
     for (var x = 0; x < count; x++) {
       for (var y = 0; y < count; y++) {
         cubes.push({
-          shape: new Isomer.Shape.Prism(new Isomer.Point(x * size, y * size, 0), size, size, size),
+          shape: new Isomer.Shape.Prism(
+            new Isomer.Point(
+              x * size + initialX,
+              y * size + initialY,
+              initialZ
+            ),
+            size,
+            size,
+            size
+          ),
           delay1: (x + y) / delayFactor1,
-          delay2: (x - y) / delayFactor2
+          delay2: (x - y) / delayFactor2,
         });
       }
     }
 
     // Function to get color based on amplitude
     function getColor(amplitude) {
-      var r = Math.round((amplitude + 1) / 2 * 255);
+      var r = Math.round(((amplitude + 1) / 2) * 255);
       var b = Math.round((1 - (amplitude + 1) / 2) * 255);
-      var a = r < b ? 0.9 : 1 / (r/b);
-      return new Isomer.Color(b, Math.abs(r-b), r, a);
+      var a = r < b ? 0.9 : 1 / (r / b);
+      return new Isomer.Color(b, Math.abs(r - b), r, a);
     }
 
     // Animation loop
@@ -55,7 +66,9 @@ export default function Home() {
 
       // Draw cubes with delay
       for (var i = cubes.length - 1; i >= 0; i--) {
-        var tz = Math.sin(t - cubes[i].delay1 + 0.06 * i) * amplitude1 + Math.sin(t - cubes[i].delay2 + 0.05 * i) * amplitude2;
+        var tz =
+          Math.sin(t - cubes[i].delay1 + 0.06 * i) * amplitude1 +
+          Math.sin(t - cubes[i].delay2 + 0.05 * i) * amplitude2;
         var color = getColor(tz / (2 * amplitude2));
         iso.add(cubes[i].shape.translate(0, 0, tz), color);
       }
@@ -69,33 +82,10 @@ export default function Home() {
 
     // Start animation
     animate();
+  }, []);
 
-  },[]);
+
   
-
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
-
-  function setValue(event) {
-    let number = event.target.valueAsNumber;
-    console.log(number);
-    switch(event.target.id){
-      case 'red_input':
-        setValue1(number)
-        val1 = number
-        break;
-      case 'blue_input':
-        setValue2(number)
-        break;
-      case 'green_input':
-        setValue3(number)
-        break;
-    }
-  }
-
   return (
     <div className={styles.container}>
       <Head>
@@ -108,7 +98,7 @@ export default function Home() {
         <PageChunk>
           <div className={styles.landing_grid}>
             <div className={styles.canvas_container}>
-              <canvas id="box_canvas" height="600" width="600"></canvas>
+              <canvas id="box_canvas" height="390" width="390"></canvas>
               {/* <canvas id="pyramid_canvas" height="600" width="600"></canvas> */}
             </div>
             <div className={styles.info_container}>
